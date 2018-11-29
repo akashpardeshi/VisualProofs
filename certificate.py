82b5f1bb56adf1f601a94d99b8ad7979c0934624
Akash Pardeshi <akash.pardeshi@gmail.com>
2018-11-29 11:01:05 -0600

    Change variable names

--- certificate.py
+++ certificate.py
@@ -3,53 +3,53 @@ Test for primality and generate a Pratt Certificate for primes
 '''
 
 def main ():
-    N = int ( input("Enter a number: ") )
-    primeFactors = factorize ( N-1, set() )
+    N = int ( input ( "Enter a number: " ) )
+    primeFactors = factorize ( N - 1, set() )
 
     if N == 2:
         print ( "2 is prime" )
-    elif (witness (N, primeFactors) == -1):
+    elif ( witness ( N, primeFactors ) == -1 ):
         print ( "%d is not prime" % ( N ) )
     else:
         print ( "%d -- %d" % ( N, witness ( N, primeFactors ) ) )
         certificate ( N, primeFactors, "    " )
 
-def factorize ( number, primeFactors ):
+def factorize ( N, primeFactors ):
     '''
     Return a sorted list of the prime factors of a given number
-    :param: number:       the number to be prime factorized
+    :param: N:            the number to be prime factorized
     :param: primeFactors: list containing the prime factors of number
     '''
     factor = 3
-    if number == 1:
+    if N == 1:
         return sorted ( list ( primeFactors ) )
-    elif number % 2 == 0:
+    elif N % 2 == 0:
         primeFactors.add(2)
-        return factorize ( number // 2, primeFactors )
+        return factorize ( N // 2, primeFactors )
     else:
-        while number % factor != 0:
+        while N % factor != 0:
             factor += 2
         primeFactors.add(factor)
-        return factorize ( number // factor, primeFactors )
+        return factorize ( N // factor, primeFactors )
 
-def witness ( number, primeFactors ):
+def witness ( N, primeFactors ):
     '''
     test for primality and if the input is prime, generate a witness
-    :param: number      : the number to test for primality and generate a witness for 
+    :param: N           : the number to test for primality and generate a witness for 
     :param: primeFactors: a list of the prime factors of number - 1
     '''
-    wit = 2
-    if wit ** (number - 1) % number != 1:
+    a = 2
+    if a ** (N - 1) % N != 1:
         return -1 
     else: 
-        while ( 1 in [wit ** (number // k) % number for k in primeFactors] ):
-            wit += 1
-    return wit 
+        while ( 1 in [a ** ( (N - 1) // k) % N for k in primeFactors] ):
+            a += 1
+    return a 
 
-def certificate ( number, primeFactors, space ):
+def certificate ( N, primeFactors, space ):
     '''
     generate a Pratt Certificate for a prime number
-    :param: number:       the number to be issued a certificate
+    :param: N:            the number to be issued a certificate
     :param: primeFactors: list of the prime factors of number - 1
     :param: space:        formatting string of empty space
     '''
