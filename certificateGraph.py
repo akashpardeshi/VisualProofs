92b04007518d7e1de775fbaa189c211befdbed2e
Akash Pardeshi <akash.pardeshi@gmail.com>
2018-12-08 21:46:55 -0600

    Add comments

--- certificateGraph.py
+++ certificateGraph.py
@@ -1,13 +1,17 @@
 from graphviz import Digraph
-import sys
+# import sys
+
+'''
+Test for primality and generate a directed graph Pratt Certificate for primes
+'''
 
 def main ():
-    if len( sys.argv) != 2:
-        print ("Only provide one argument")
-        exit()
+#     if len( sys.argv) != 2:
+#         print ("Only provide one argument")
+#         exit()
 
-    N = int ( sys.argv[1] )
-#    N = int ( input ("Enter a number: ") )
+#     N = int ( sys.argv[1] )
+    N = int ( input ("Enter a number: ") )
     primeFactors = factorize ( N - 1, [] )
 
     g = Digraph ('G', filename = 'sample-output/certificateGraph_' + '%s'%N + '.gv')
@@ -20,6 +24,11 @@ def main ():
         g.view()
 
 def factorize ( N, primeFactors ):
+    '''
+    Return a sorted list of the prime factors of a given number
+    :param: N:            the number to be prime factorized
+    :param: primeFactors: list (with duplicates) containing the current known prime factors of N
+    '''
     factor = 3
     if N == 1:
         return primeFactors
@@ -33,6 +42,11 @@ def factorize ( N, primeFactors ):
         return factorize ( N // factor, primeFactors )
 
 def witness ( N, primeFactors ):
+    '''
+    test for primality and if the input is prime, generate a (number, witness) tuple
+    :param: N           : the number to test for primality and generate a witness for
+    :param: primeFactors: a list of the prime factors of N - 1
+    '''
     a = 2
     if N == 2:
         return ( 2, 1 )
@@ -44,23 +58,38 @@ def witness ( N, primeFactors ):
     return ( N, a )
 
 def subLayer ( N, primeFactors ):
+    '''
+    Compute the set of nodes eminating from a parent node
+    :param: N          : the parent node
+    :param primeFactors: a list of the prime factors of N - 1
+    '''
     child = set()
     for i in primeFactors:
         child.add( witness (i, factorize (i - 1, [] ) ) )
     return sorted ( list ( child ) )
 
 def graph ( N, primeFactors, parent, child, g, existingNodes ):
+    '''
+    Draw the connections from a parent node to all children nodes
+    :param: N            : the number to generate the graph of 
+    :param: primeFactors : a list of the prime factors of N - 1
+    :param: parent       : the parent tuple node
+    :param: child        : a list of all children tuple nodes
+    :param: g            : the existing graph
+    :param: existingNodes: a list of the numbers on which we
+                           have already recursed on
+    '''
     for pair in child:
         childN = pair[0]
         childFactors = factorize ( childN - 1, [] )
         for i in range ( primeFactors.count(childN) ):
             g.edge ( '%s' %(parent,), '%s' %(pair,) )
 
-        if childN not in existingNodes:
+        if childN not in existingNodes: # statement to avoid drawing edges from tuples which have already had
+                                        # outgoing arcs
             graph ( childN, childFactors, pair, subLayer ( childN, childFactors ), g, existingNodes )
             existingNodes.append(childN)
 
 
 main()
-
 
